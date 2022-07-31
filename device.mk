fe24381eba3537b92b054847bcfa342f59891cda
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-07-31 05:59:59 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -318,6 +318,11 @@ PRODUCT_PACKAGES += \
    SystemUIOverlayVeux \
    TelephonyOverlayVeux \
    WifiOverlayVeux
+   
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
